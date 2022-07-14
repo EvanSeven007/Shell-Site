38b4884529b78e0b7eeedac23d65a12dd6965afe
EvanSeven007 <evanstegall123@gmail.com>
2022-07-14 09:29:42 -0500

    changed some formatting

--- src/components/TerminalOutput.js
+++ src/components/TerminalOutput.js
@@ -12,7 +12,9 @@ const start_message =
                                                                                                                                  
                                                                                                                                         
                                                                                                                                         
-                                                                                                                                        
+const openUrl = async (url) => {
+    setTimeout(() => window.open(url, url), "2000");
+};                                                                                                                                
 
 //const commands = ["about", "email", "github", "help", "hello", "linkedin", "projects", "resume", "start", "time"];
 
@@ -27,7 +29,7 @@ class TerminalOutput extends Component {
         return d.toLocaleTimeString();
     }
 
-    handleCommand(cmd) {
+    handleCommand(cmd, isLastIndex) {
         switch (cmd.toLowerCase()) {
             case "":
                 return <div></div>
@@ -37,8 +39,8 @@ class TerminalOutput extends Component {
                     <p>First off, thank you for visiting my website, whoever you may be. Feel free to take a look around!</p>
                     <p>Hi! My name is Evan Stegall and I am a Junior Computer Science Student at Rice University</p>
                     <p>I have been hooked on programming and problem solving ever since I took my first Python course in high school, and since then I have
-                        been in love with all things computer science. My current career goals includes learning as much as I can about tech, both as a future Software Engineer and as a software hobbyist.
-                    </p>
+                        been in love with all things computer science.</p>
+                    <p>My current career goals includes learning as much as I can about tech, both as a future Software Engineer and as a software hobbyist.</p>
                     <p>In my free time, I enjoy climbing, powerlifting, reading, and listening to as much music as I can.</p>
                     <p>Feel free to connect with any of my contacts! (the 'help' command may prove useful)</p>
                 </span>)
@@ -50,9 +52,10 @@ class TerminalOutput extends Component {
                     <div class = "output-box"><a href="mailto: evanstegall123@gmail.com" target = "_blank" rel="noreferrer">evanstegall123@gmail.com</a></div>
                 )
             case "github":
-                return (
-                    <div class = "output-box">Github Link: <a href = "https://github.com/EvanSeven007">EvanSeven007</a></div>
-                )
+                if (isLastIndex) {
+                    openUrl("https://github.com/EvanSeven007");
+                };
+                return <div>Opening Github...</div>
             case "help":
                 return (
                     <div class = "output-box">
@@ -65,23 +68,23 @@ class TerminalOutput extends Component {
             case "hello":
                 return <div class = "output-box">Hi!</div>
             case "linkedin":
-                    return (
-                        <div class = "output-box"><a href = "https://www.linkedin.com/in/evan-stegall/">www.linkedin.com/in/evan-stegall/</a></div>
-                    )
+                if (isLastIndex) {
+                    openUrl("https://www.linkedin.com/in/evan-stegall/");
+                };
+                return <div>Opening Linkedin...</div>
             case "projects":
                 return (
                     <div class = "output-box">
                         <p><a href = "https://github.com/EvanSeven007/ReeseBot">Reese Bot</a> - A playable chess engine built in Rust using the minimax algorithm with alpha-beta pruning</p>
-                        <p><a href = "https://devpost.com/software/hfh-data-boys">The Missing Link</a> - Worked on a team of four to create a link prediction program for a given social network, winning 2nd best undergraduate team overall. 
-                        Using pandas for data processing and tensorflow for data analysis, we implemented and trained a random-forest classifier, eventually getting around 70.4% precision despite having a highly sparse training graph.</p>
+                        <p><a href = "https://devpost.com/software/hfh-data-boys">The Missing Link</a> - Worked on a team of four to create a link prediction program for a given social network, winning 2nd best undergraduate team overall.</p>
+                        <p> - Using pandas for data processing and tensorflow for data analysis, we implemented and trained a random-forest classifier, eventually getting around 70.4% precision despite having a highly sparse training graph.</p>
                     </div>
                 )
             case "resume":
-                return (
-                    <div class = "output-box">
-                        <a href = "resume.pdf" target = "_blank">resume</a>
-                    </div>
-                )
+                if (isLastIndex) {
+                    openUrl("resume.pdf");
+                };
+                return <div>Opening Resume...</div>;
             case "start":
                 return <div class = "start_banner">
                     <p>{start_message}</p>
@@ -107,7 +110,7 @@ class TerminalOutput extends Component {
             <span class = "prompt-stuff">@</span>
             <span class = "email">evanstegall.com</span>
             <span class = "prompt-stuff">$ ~ </span>
-            <span class = "output-area">{o}{this.handleCommand(o)}</span>
+            <span class = "output-area">{o}{this.handleCommand(o, key == this.props.commands.length - 1)}</span>
         </div>);
         return (
             <>{outputList}</>
